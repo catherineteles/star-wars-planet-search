30dd624c83638d9fa6ee0ced548c46fdabeed0ac
cathe <cacateles12@gmail.com>
2022-04-18 20:22:11 -0300

    Avoid repetitive filters added

--- src/components/FiltersContainer.js
+++ src/components/FiltersContainer.js
@@ -1,15 +1,20 @@
 import React, { useContext } from 'react';
 import APIContext from '../context/APIcontext';
 
+const columnOptions = ['population',
+  'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
 function FiltersContainer() {
   const {
     column,
     comparison,
     value,
     filterByName,
+    filterByNumericValues,
     updateFilter,
     updateNumericFilter,
   } = useContext(APIContext);
+
+  const checkColumn = (element, filter) => element !== filter.column;
 
   return (
     <div>
@@ -25,11 +30,19 @@ function FiltersContainer() {
         name="column"
         data-testid="column-filter"
       >
-        <option value="population">population</option>
+        {columnOptions.filter((element) => {
+          if (filterByNumericValues.length !== 0) {
+            return filterByNumericValues.every((filter) => checkColumn(element, filter));
+          }
+          return element;
+        }).map((element) => (
+          <option key={ element } value={ element }>{element}</option>
+        ))}
+        {/* <option value="population">population</option>
         <option value="orbital_period">orbital_period</option>
         <option value="diameter">diameter</option>
         <option value="rotation_period">rotation_period</option>
-        <option value="surface_water">surface_water</option>
+        <option value="surface_water">surface_water</option> */}
       </select>
       <select
         value={ comparison }
